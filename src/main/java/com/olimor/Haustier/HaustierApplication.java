379e0067b114dba7336a82bc5d5c4994d023509c
abnerolimor <abner.oliveira.moraes@escola.pr.gov.br>
2025-06-27 10:37:07 -0300

    projetos

---
+++ src/main/java/com/olimor/Haustier/HaustierApplication.java
@@ -0,0 +1,13 @@
+package com.olimor.Haustier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HaustierApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HaustierApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/olimor/Haustier/HaustierApplicationTests.java
@@ -0,0 +1,13 @@
+package com.olimor.Haustier;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HaustierApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
